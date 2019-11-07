1f839872844a331bee47a2174eacd5114f7ed706
BernardoFaria <bernardo.faria@tecnico.ulisboa.pt>
2019-11-07 22:52:46 +0000

    Hashtable feita; semaforos incompletos

--- Projeto2/fs.c
+++ Projeto2/fs.c
@@ -15,58 +15,67 @@ int obtainNewInumber(tecnicofs* fs) {
 	return newInumber;
 }
 
-tecnicofs* new_tecnicofs(){
+tecnicofs* new_tecnicofs(int numBuckets){
 	tecnicofs*fs = malloc(sizeof(tecnicofs));
 	if (!fs) {
 		perror("failed to allocate tecnicofs");
 		exit(EXIT_FAILURE);
 	}
-	fs->bstRoot = NULL;
+	fs->bstRoot = malloc(sizeof(node **)*numBuckets);
+	fs->bstLock = malloc(sizeof(syncMech *)*numBuckets);
 	fs->nextINumber = 0;
-	sync_init(&(fs->bstLock));
+
+
+
+	for(int i = 0; i < numBuckets; i++) {
+		fs->bstRoot[i] = NULL;
+		sync_init(&(fs->bstLock[i]));
+	}
 	return fs;
 }
 
 
 
 
-void free_tecnicofs(tecnicofs* fs){
-	free_tree(fs->bstRoot);
-	sync_destroy(&(fs->bstLock));
+void free_tecnicofs(tecnicofs* fs, int numBuckets) {
+	for(int i = 0; i < numBuckets; i++) {
+		free_tree(fs->bstRoot[i]);
+		sync_destroy(&(fs->bstLock[i]));
+	}
 	free(fs);
 }
 
 
 
 
-// void create(tecnicofs* fs, hashTable *hashtable, int hashIndex, char *name, int inumber){
-void create(tecnicofs* fs, char *name, int inumber){
-	sync_wrlock(&(fs->bstLock));
-	fs->bstRoot = insert(fs->bstRoot, name, inumber);
+// void create(tecnicofs* fs, char *name, int inumber){
+void create(tecnicofs* fs, char *name, int inumber, int hashIdx){
+	sync_wrlock(&(fs->bstLock[hashIdx]));
+	fs->bstRoot[hashIdx] = insert(fs->bstRoot[hashIdx], name, inumber);
 	// hashInsert(hashtable, fs->bstRoot, name);
-	sync_unlock(&(fs->bstLock));
+	sync_unlock(&(fs->bstLock[hashIdx]));
 }
 
 
 
 
-void delete(tecnicofs* fs, char *name){
-	sync_wrlock(&(fs->bstLock));
-	fs->bstRoot = remove_item(fs->bstRoot, name);
-	sync_unlock(&(fs->bstLock));
+void delete(tecnicofs* fs, char *name, int hashIdx){
+	sync_wrlock(&(fs->bstLock[hashIdx]));
+	fs->bstRoot[hashIdx] = remove_item(fs->bstRoot[hashIdx], name);
+	sync_unlock(&(fs->bstLock[hashIdx]));
 }
 
 
 
 
-int lookup(tecnicofs* fs, char *name){
-	sync_rdlock(&(fs->bstLock));
+int lookup(tecnicofs* fs, char *name, int hashIdx){
+	sync_rdlock(&(fs->bstLock[hashIdx]));
 	int inumber = 0;
-	node* searchNode = search(fs->bstRoot, name);
+	node* searchNode = search(fs->bstRoot[hashIdx], name);
 	if ( searchNode ) {
 		inumber = searchNode->inumber;
 	}
-	sync_unlock(&(fs->bstLock));
+	sync_unlock(&(fs->bstLock[hashIdx]));
 	return inumber;
 }
 
@@ -74,23 +83,25 @@ int lookup(tecnicofs* fs, char *name){
 
 
 
-void renameFile(tecnicofs* fs, char* name, char* newName) {
-	// sync_wrlock(&(fs->bstLock));
-	// node* searchName = search(fs->bstRoot, name);
-	// node* searchNewName = search(fs->bstRoot, newName);
+void renameFile(tecnicofs* fs, char* name, char* newName, int hashIdx) {
+	sync_wrlock(&(fs->bstLock[hashIdx]));
+	node* searchName = search(fs->bstRoot[hashIdx], name);
+	node* searchNewName = search(fs->bstRoot[hashIdx], newName);
 
-	// if(searchName != NULL && searchNewName == NULL) {
-	// 	int iNumber = fs->bstRoot->inumber;
-	// 	delete(fs, name);
-	// 	create(fs, newName, iNumber);
-	// }
-	// sync_unlock(&(fs->bstLock));
+	if(searchName != NULL && searchNewName == NULL) {
+		int iNumber = fs->bstRoot[hashIdx]->inumber;
+		delete(fs, name, hashIdx);
+		create(fs, newName, iNumber, hashIdx);
+	}
+	sync_unlock(&(fs->bstLock[hashIdx]));
 }
 
 
 
 
 
-void print_tecnicofs_tree(FILE * fp, tecnicofs *fs){
-	print_tree(fp, fs->bstRoot);
+void print_tecnicofs_tree(FILE * fp, tecnicofs *fs, int numBuckets){
+	for(int i = 0; i < numBuckets; i++) {
+		print_tree(fp, fs->bstRoot[i]);
+	}
 }
