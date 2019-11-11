da636515ef9ad5864a133798dac7cbfbacbc5b1d
BernardoFaria <bernardo.faria@tecnico.ulisboa.pt>
2019-11-11 14:45:20 +0000

    shell script feita, muitas threads da infinite loop ou seg fault

--- Projeto2/runTests.sh
+++ Projeto2/runTests.sh
@@ -4,10 +4,26 @@
 # NThreads=$3
 # NBoquetes=$4
 
-for test in $1/* do
-    ./tecnicofs-nosync $test "$test-1.txt" 1 1
+# nameRight = test1
+# test = inputs/test1.txt
 
-    for thread in $(seq 2 $3) do
-        ./tecnicofs-mutex $test "$test-$thread.txt" $thread $4
+
+mkdir -p $2
+
+for test in $1/* 
+do
+
+    nameRight=${test#"$1/"}
+    nameRight=${nameRight%".txt"}
+    
+    echo InputFile=​ $test ​ NumThreads=​ 1
+    ./tecnicofs-nosync $test "$2/$nameRight-1.txt" 1 1 | tail -1
+
+
+    for thread in $(seq 2 $3) 
+    do
+        echo InputFile=​ $test ​ NumThreads=​ $thread
+        ./tecnicofs-mutex $test "$2/$nameRight-$thread.txt" $thread $4 | tail -1
+
     done
 done
